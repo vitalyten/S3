1df6bec09c3800fac98030d6c083e5a03b932faa
Nicolas Humbert <nicolas.humbert@live.fr>
2017-08-09 15:09:53 -0700

    bash

---
+++ testsbash.bash
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+if [ $CIRCLE_NODE_INDEX -eq 0 ] then
+
+  npm run --silent lint -- --max-warnings 0
+
+  npm run --silent lint_md
+
+  flake8 $(git ls-files "*.py")
+
+  yamllint $(git ls-files "*.yml")
+
+  mkdir -p $CIRCLE_TEST_REPORTS/unit
+
+  npm run unit_coverage
+
+  npm run start_dmd &
+  bash wait_for_local_port.bash 9990 40 &&
+  npm run multiple_backend_test
+
+  S3BACKEND=mem S3DATA=multiple npm start
+  > $CIRCLE_ARTIFACTS/server_multiple_java.txt
+  & bash wait_for_local_port.bash 8000 40
+  && cd ./tests/functional/jaws && mvn test
+
+  S3BACKEND=mem S3DATA=multiple npm start
+  $CIRCLE_ARTIFACTS/server_multiple_fog.txt
+  & bash wait_for_local_port.bash 8000 40
+  && cd tests/functional/fog && rspec tests.rb
+
+fi
+
+exit 0
