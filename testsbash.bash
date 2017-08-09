556459754159db8b5c9360f295de0b737a7cade4
Nicolas Humbert <nicolas.humbert@live.fr>
2017-08-09 15:40:18 -0700

    fix

--- testsbash.bash
+++ testsbash.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
-if [ $CIRCLE_NODE_INDEX -eq 0 ] then
+if [ $CIRCLE_NODE_INDEX -eq 0 ]
+then
 
   npm run --silent lint -- --max-warnings 0
 
@@ -18,17 +19,11 @@ if [ $CIRCLE_NODE_INDEX -eq 0 ] then
   bash wait_for_local_port.bash 9990 40 &&
   npm run multiple_backend_test
 
-  S3BACKEND=mem S3DATA=multiple npm start
-  > $CIRCLE_ARTIFACTS/server_multiple_java.txt
-  & bash wait_for_local_port.bash 8000 40
-  && cd ./tests/functional/jaws && mvn test
+  S3BACKEND=mem S3DATA=multiple npm start > $CIRCLE_ARTIFACTS/server_multiple_java.txt & bash wait_for_local_port.bash 8000 40 && cd ./tests/functional/jaws && mvn test
 
   kill -9 $(lsof -t -i:8000)
 
-  S3BACKEND=mem S3DATA=multiple npm start
-  $CIRCLE_ARTIFACTS/server_multiple_fog.txt
-  & bash wait_for_local_port.bash 8000 40
-  && cd tests/functional/fog && rspec tests.rb
+  S3BACKEND=mem S3DATA=multiple npm start > $CIRCLE_ARTIFACTS/server_multiple_fog.txt & bash wait_for_local_port.bash 8000 40 && cd tests/functional/fog && rspec tests.rb
 
 fi
 
